12e588c9e260fa239cb0be24c2a6ba8573e63094
hyunbin1 <kimgusqls1@gmail.com>
2021-04-11 12:57:01 +0900

    create styles.css lots to use css in images or and so on

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// npm install @material-ui/core 사용하기 
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import memories from './images/Memories.jpg';
+import useStyles from './styles';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const classes = useStyles();
+    return (
+        <container maxwidth="lg">
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} varaint="h2" align="center"></Typography>
+                <img className={classes.image} src={memories} alt="memories" height="60"></img>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+
+
+            </Grow>
+        </container>
+
+
+
+
+
+    );
 }
+
 
 export default App;
